0f5da07b4c85de989b175311c772b956a1f46671
Sachin <sachin.mishra@datasociety.com>
2023-02-25 00:37:46 +0530

    path added

--- youtube.py
+++ youtube.py
@@ -14,9 +14,12 @@ def random_emoji():
 url = st.text_input('Please enter Video URL')
 st.write('Your entered URL', url)
 
+path = st.text_input('Give path to store')
+st.write('Your entered Path', path)
+
 if st.button('Download Video'):
     video=YouTube(url)
     stream=video.streams.get_highest_resolution()
     st.write('downloading..please wait!',random_emoji())
-    stream.download()
+    stream.download(path)
     st.write('Hurrah!! Video downloaded!',random_emoji())
