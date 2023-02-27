dd56311049ebca3768373ee3b6eab15a5688ef44
Sachin <sachin.mishra@datasociety.com>
2023-02-27 17:33:23 +0530

    functionality added

--- youtube.py
+++ youtube.py
@@ -1,25 +1,48 @@
 import streamlit as st
 from pytube import YouTube
 import random
+import re
+import os
+from zipfile import ZipFile
+
 
 emojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼"]
 def random_emoji():
     st.session_state.emoji = random.choice(emojis)
     return st.session_state.emoji
 
+def zip_csv(directory_name, zip_file_name, filter):
+   # Create object of ZipFile
+   with ZipFile(zip_file_name, 'w') as zip_object:
+   # Traverse all files in directory
+    for folder_name, sub_folders, file_names in os.walk(directory_name):
+      for filename in file_names:
+      # Filter for csv files
+       if filter(filename):
+         # Create filepath of files in directory
+         file_path = os.path.join(folder_name, filename)
+         # Add files to zip file
+         zip_object.write(file_path, os.path.basename(file_path))
 
+with st.sidebar:
+    choice= st.radio("Navigation",["Provide Url",'download'])
 """
 # Youtube Video Downloader
 """
-url = st.text_input('Please enter Video URL')
-st.write('Your entered URL', url)
+if choice == 'Provide Url':
+    url = st.text_input('Please enter Video URL')
+    st.write('Your entered URL', url)
+    if url:
+        video=YouTube(url)
+        stream=video.streams.get_highest_resolution()
+        st.write('downloading..please wait!',random_emoji())
+        stream.download()
+        zip_csv('./', './Zipped file.zip', lambda name: 'mp4' in name)
+        st.write('Success!',random_emoji())
 
-path = st.text_input('Give path to store')
-st.write('Your entered Path', path)
-
-if st.button('Download Video'):
-    video=YouTube(url)
-    stream=video.streams.get_highest_resolution()
-    st.write('downloading..please wait!',random_emoji())
-    stream.download(path)
-    st.write('Hurrah!! Video downloaded!',random_emoji())
+if choice == 'download':
+    if os.path.exists('./Zipped file.zip'):
+        with open("Zipped file.zip",'rb') as f:
+            st.download_button('download_video',f,file_name="Zipped file.zip")
+    else:
+       st.write('Go to provide Url button')
